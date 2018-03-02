ab02ba3ba9c91def969f005b3d047fc1afd4365c
steven <751836594@qq.com>
2018-03-02 15:54:34 +0800

    supervisor

--- src/app/tasks/DemoTask.php
+++ src/app/tasks/DemoTask.php
@@ -14,8 +14,26 @@ class DemoTask extends Task
 {
     public function testAction()
     {
-        echo 1 . "\n";
-        exit();
+        $http = new \swoole_http_server("0.0.0.0", 9501);
+
+        $http->on("start", function ($server) {
+            echo "Swoole http server is started at http://127.0.0.1:9501\n";
+        });
+
+        $http->on("request", function ($request, $response) {
+            $response->header("Content-Type", "text/plain");
+            $response->end("Hello World\n");
+        });
+
+        $http->start();
+    }
+
+
+    public function demoAction()
+    {
+        $code =['270540063','270540064','270540065','270540065'];
+        $a = array_count_values($code);
+        print_r($a);
     }
 
 }
